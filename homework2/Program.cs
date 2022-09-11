660c2d46a43d8afa4016af53f390ab3242048a12
D <kritantablake@gmail.com>
2022-09-11 17:10:50 +0300

    task 2 + result photo

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -4,11 +4,18 @@
 
 for(int i = 0; i < 20; i++) //micropool
 {
+    //ThreadPool.SetMinThreads(23, 23);     it's just for test
+    ThreadPool.SetMaxThreads(30, 30);
+    
     ThreadPool.QueueUserWorkItem(new WaitCallback(SomeLogicClass.SomeLogicMethod));
 }
 
 Thread.Sleep(200);
 
+if (ThreadPool.ThreadCount > 20)    //i haven't found better place for check count of threads than here 
+{
+    throw new Exception("hey its more than 20");
+}
 
 Console.WriteLine($"\n Count: {ThreadPool.ThreadCount}");
 Console.WriteLine($"\n Complete: {ThreadPool.CompletedWorkItemCount}");
