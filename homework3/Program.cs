c0f0196b11b8b1604ab2a3bd175eefc830954116
D <kritantablake@gmail.com>
2022-09-12 21:19:50 +0300

    task2 + result photo
    вроде как правильно реализовал

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -5,30 +5,32 @@ Random rnd =new();
 
 for (int i = 0; i < 40; i++)
 {
+    var employeeInsideSalary = EmployeesFactory.SalaryInside<Insider>();
+
+    employeeInsideSalary
+        .AddName(Convert.ToString((Names)rnd.Next(1, 20)))
+        .AddSurname(Convert.ToString((Surnames)rnd.Next(1, 20)));
+
     
-    var employeeInside = EmployeesFactory.SalaryInside<Insider>(
-        Convert.ToString((Names)rnd.Next(1, 20)), 
-        Convert.ToString((Surnames)rnd.Next(1, 20)));
-    
-    
-    var employeeOutside = EmployeesFactory.SalaryOutside<Outsider>(
-        Convert.ToString((Names)rnd.Next(1, 20)), 
-        Convert.ToString((Surnames)rnd.Next(1, 20)),
+    var employeeOutsideSalary = EmployeesFactory.SalaryOutside<Outsider>(
         rnd.Next(1000, 2000), 
         rnd.Next(10, 20),
         rnd.Next(5, 9));
     
+    employeeOutsideSalary
+        .AddName(Convert.ToString((Names)rnd.Next(1, 20)))
+        .AddSurname(Convert.ToString((Surnames)rnd.Next(1, 20)));
     
     
-    Console.WriteLine($"Employee\n" +    //это д/з 1; немного не понял, как заполнить без фабрики(если только !?напрямую, но за такое и по рукам можно получить)
-                      $"Name: {employeeInside.Name}\n" +
-                      $"Surname: {employeeInside.Surname}\n" +
-                      $"Company: {employeeInside.Company}\n" +
-                      $"Salary: {employeeInside.MonthSalary}\n");
+    Console.WriteLine($"Employee\n" +
+                      $"Name: {employeeInsideSalary.Name}\n" +
+                      $"Surname: {employeeInsideSalary.Surname}\n" +
+                      $"Company: {employeeInsideSalary.Company}\n" +
+                      $"Salary: {employeeInsideSalary.MonthSalary}\n");
     
     Console.WriteLine($"Freelancer\n" +
-                      $"Name: {employeeOutside.Name}\n" +
-                      $"Surname: {employeeOutside.Surname}\n" +
-                      $"Company: {employeeOutside.Company}\n" +
-                      $"Salary: {employeeOutside.MonthSalary}\n");
+                      $"Name: {employeeOutsideSalary.Name}\n" +
+                      $"Surname: {employeeOutsideSalary.Surname}\n" +
+                      $"Company: {employeeOutsideSalary.Company}\n" +
+                      $"Salary: {employeeOutsideSalary.MonthSalary}\n");
 }
