ab9bd501048a5b223d0f67f6836d50aef2b14f02
D <kritantablake@gmail.com>
2022-09-18 20:33:29 +0300

    add code

---
+++ homework5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using ConsoleApp1;
+
+//немного непонятно в методичке указана задача, но, вроде, реализовал правильно
+
+LoadData data = new();
+SomeLibrary someLibrary = new();
+
+byte[] load = data.DisplayData("");
+byte[] res = data.GetFile("").Result;
+    
+someLibrary.WriteData(res);
+someLibrary.WriteData(load);
+
+Console.ReadKey();
