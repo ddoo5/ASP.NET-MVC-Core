cfbafd3a604e56220496e54be56209ffb554248f
D <kritantablake@gmail.com>
2022-09-28 19:34:00 +0300

    generator

---
+++ homework7/Reports/ReportsGenerator/ProductGeneratorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WebApplication1.DB.Models;
+
+namespace WebApplication1.Reports.Models
+{
+    public class ProductGeneratorModel
+    {
+        public string Name { get; set; } = null!;
+        public int Bill { get; set; } = 0;
+        public string Address { get; set; } = null!;
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
+
