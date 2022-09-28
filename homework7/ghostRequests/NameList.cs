8cbeae89e8b98904ea9b803b2d426f960c984fce
D <kritantablake@gmail.com>
2022-09-28 19:34:41 +0300

    old code

---
+++ homework7/ghostRequests/NameList.cs
@@ -0,0 +1,15 @@
+namespace WebApplication1.HelperServices;
+
+public enum NameList
+{
+    dishwasher = 1,
+    MoonlightSonata_Beethoven_in_B_minor,
+    Tesla,
+    MSI,
+    GTX_1080,
+    NLP,
+    Samsung,
+    Iphone,
+    Scarlett_comfort,
+    Sony_ps4
+}
